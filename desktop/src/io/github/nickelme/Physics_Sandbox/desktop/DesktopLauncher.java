9a97e1e20354f8bca299978f35bacc77aef669f4
Nickelme <nickelbot5.3@gmail.com>
2014-11-12 11:42:59 -0500

    Getting started

---
+++ desktop/src/io/github/nickelme/Physics_Sandbox/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.github.nickelme.Physics_Sandbox.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.github.nickelme.Physics_Sandbox.PhysicsSandboxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PhysicsSandboxGame(), config);
+	}
+}
